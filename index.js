e6639e2ce000a29ef329ed703ba9c52a7143f696
Diego <diego.afonso@ecole-isitech.fr>
2023-03-29 11:03:36 +0200

    commit js 1103

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-app.use()
-
 app.get('/', function(httpRequest,httpResponse){
     console.log('objet request: ', httpRequest);
     httpresponse.render('pages/index')
 });
+
+app.get('/toto', function(httpRequest,httpResponse){
+    console.log('objet request: ', httpRequest);
+    httpresponse.send('route toto')
+});
+
+app.get('/about', (req,res) => {
+    res.render('pages/about')
+})
 
 app.listen('3003')
