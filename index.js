4c3b4d78d0379d0c5b1bcd487818b0ae57e7e4a3
Diego <diego.afonso@ecole-isitech.fr>
2023-04-04 08:47:17 +0200

    commit work 0847

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ app.get('/', function(req, res) {
         console.log(result)
         res.render('pages/index', {users: result})
     })
-})
+});
 
 app.post('/cart', function(req, res ){
     console.log('req after add to cart: ', req.body)
-})
+});
 
 app.get('/', function(httpRequest,httpResponse){
     console.log('objet request: ', httpRequest);
@@ -41,58 +41,58 @@ app.get('/toto', function(httpRequest,httpResponse){
 
 app.get('/about', (req,res) => {
     res.render('pages/about')
-})
+});
 
 app.listen('3003')
 
 app.get('/panier', (req, res) => {
     res.render('pages/panier');
-})
+});
 
 app.get('/VAnkara', (req, res) => {
     res.render('pages/VAnkara');
-})
+});
 
 app.get('/VBarcelone', (req, res) => {
     res.render('pages/VBarcelone');
-})
+});
 
 app.get('/VLisbonne', (req, res) => {
     res.render('pages/VLisbonne');
-})
+});
 
 app.get('/VLondres', (req, res) => {
     res.render('pages/VLondres');
-})
+});
 
 app.get('/VLyon', (req, res) => {
     res.render('pages/VLyon');
-})
+});
 
 app.get('/VMarrakech', (req, res) => {
     res.render('pages/VMarrakech');
-})
+});
 
 app.get('/VParis', (req, res) => {
     res.render('pages/VParis');
-})
+});
 
 app.get('/VPorto', (req, res) => {
     res.render('pages/VPorto');
-})
+});
 
 app.get('/VRome', (req, res) => {
     res.render('pages/VRome');
-})
+});
 
 app.get('/VSeoul', (req, res) => {
     res.render('pages/VSeoul');
-})
+});
 
 app.get('/VSingapour', (req, res) => {
     res.render('pages/VSingapour');
-})
+});
 
 app.get('/VTokyo', (req, res) => {
     res.render('pages/VTokyo');
-})
+});
